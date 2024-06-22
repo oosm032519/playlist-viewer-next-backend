da2b0c1b83719598a1d19f9f12917682cf1a51de
oosm032519 <ryooshima0519@gmail.com>
2024-06-23 05:32:28 +0900

    エラーハンドリングを強化

--- src/main/java/com/github/oosm032519/playlistviewernext/controller/PlaylistController.java
+++ src/main/java/com/github/oosm032519/playlistviewernext/controller/PlaylistController.java
@@ -3,6 +3,7 @@ package com.github.oosm032519.playlistviewernext.controller;
 import com.github.oosm032519.playlistviewernext.service.SpotifyService;
 import org.apache.hc.core5.http.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
@@ -19,14 +20,15 @@ public class PlaylistController {
     private SpotifyService spotifyService;
 
     @GetMapping("/search")
-    public ResponseEntity<List<PlaylistSimplified>> searchPlaylists(@RequestParam String query) throws ParseException {
+    public ResponseEntity<List<PlaylistSimplified>> searchPlaylists(@RequestParam String query) {
         try {
             spotifyService.getAccessToken();
             List<PlaylistSimplified> playlists = spotifyService.searchPlaylists(query);
             return ResponseEntity.ok(playlists);
-        } catch (IOException | SpotifyWebApiException e) {
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
             e.printStackTrace();
-            return ResponseEntity.internalServerError().build();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(null);
         }
     }
 }
