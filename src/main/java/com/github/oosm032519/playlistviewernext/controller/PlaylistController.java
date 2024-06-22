ab91a7a875aabf6bad243a5d3c878f5a9eec4deb
oosm032519 <ryooshima0519@gmail.com>
2024-06-23 05:35:26 +0900

    ログを強化

--- src/main/java/com/github/oosm032519/playlistviewernext/controller/PlaylistController.java
+++ src/main/java/com/github/oosm032519/playlistviewernext/controller/PlaylistController.java
@@ -2,6 +2,8 @@ package com.github.oosm032519.playlistviewernext.controller;
 
 import com.github.oosm032519.playlistviewernext.service.SpotifyService;
 import org.apache.hc.core5.http.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,17 +18,21 @@ import java.util.List;
 @RequestMapping("/api/playlists")
 public class PlaylistController {
 
+    private static final Logger logger = LoggerFactory.getLogger(PlaylistController.class); // ロガーを追加
+
     @Autowired
     private SpotifyService spotifyService;
 
     @GetMapping("/search")
     public ResponseEntity<List<PlaylistSimplified>> searchPlaylists(@RequestParam String query) {
+        logger.info("PlaylistController: searchPlaylists called with query: {}", query); // クエリをログ出力
         try {
             spotifyService.getAccessToken();
             List<PlaylistSimplified> playlists = spotifyService.searchPlaylists(query);
+            logger.info("PlaylistController: Found {} playlists", playlists.size()); // プレイリスト数をログ出力
             return ResponseEntity.ok(playlists);
         } catch (IOException | SpotifyWebApiException | ParseException e) {
-            e.printStackTrace();
+            logger.error("PlaylistController: Error searching playlists", e); // エラーをログ出力
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(null);
         }
