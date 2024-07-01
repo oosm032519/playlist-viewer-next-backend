a787f6d78c93ccc152db49b5f09f42f5296dccd3
oosm032519 <ryooshima0519@gmail.com>
2024-07-01 18:58:44 +0900

    定数導入、Map 操作の最適化、変数名の改善

--- src/main/java/com/github/oosm032519/playlistviewernext/service/CustomOAuth2UserService.java
+++ src/main/java/com/github/oosm032519/playlistviewernext/service/CustomOAuth2UserService.java
@@ -7,30 +7,26 @@ import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
+    private static final String ACCESS_TOKEN_KEY = "access_token";
+    private static final String USER_NAME_ATTRIBUTE_KEY = "id";
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User user = super.loadUser(userRequest);
-
-        // アクセストークンを取得
         String accessToken = userRequest.getAccessToken().getTokenValue();
 
-        // 新しい変更可能なマップを作成し、元の属性をコピー
-        Map<String, Object> mutableAttributes = new HashMap<>(user.getAttributes());
+        Map<String, Object> enhancedAttributes = new java.util.HashMap<>(Map.copyOf(user.getAttributes()));
+        enhancedAttributes.put(ACCESS_TOKEN_KEY, accessToken);
 
-        // アクセストークンを属性に追加
-        mutableAttributes.put("access_token", accessToken);
-
-        // 新しい DefaultOAuth2User オブジェクトを作成して返す
         return new DefaultOAuth2User(
                 user.getAuthorities(),
-                mutableAttributes,
-                "id"
+                enhancedAttributes,
+                USER_NAME_ATTRIBUTE_KEY
         );
     }
 }
