51f401a2c7a39f5e21fe751414dc2c7fa272a6ec
oosm032519 <ryooshima0519@gmail.com>
2024-07-01 18:47:33 +0900

    ログイン成功時のログ出力を改善

--- src/main/java/com/github/oosm032519/playlistviewernext/controller/LoginSuccessController.java
+++ src/main/java/com/github/oosm032519/playlistviewernext/controller/LoginSuccessController.java
@@ -4,16 +4,20 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 public class LoginSuccessController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginSuccessController.class);
 
     @GetMapping("/loginSuccess")
     public String loginSuccess(@AuthenticationPrincipal OAuth2User principal) {
         String userId = principal.getAttribute("id");
         String accessToken = principal.getAttribute("access_token");
-        System.out.println("User successfully authenticated: " + userId);
-        System.out.println("Access token: " + accessToken);
+        logger.info("User successfully authenticated: {}", userId);
+        logger.info("Access token: {}", accessToken);
 
         return "redirect:http://localhost:3000/";
     }
