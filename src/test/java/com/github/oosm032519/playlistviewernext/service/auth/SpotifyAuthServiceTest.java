7ec8ca2208d29082e3f5c27e9ef547f64bb00fa5
oosm032519 <ryooshima0519@gmail.com>
2024-07-08 02:27:14 +0900

    ファイル名を修正

--- src/test/java/com/github/oosm032519/playlistviewernext/service/auth/SpotifyAuthServiceTest.java
+++ src/test/java/com/github/oosm032519/playlistviewernext/service/auth/SpotifyAuthServiceTest.java
@@ -22,7 +22,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class SpotifyUserAuthenticationServiceTest {
+class SpotifyAuthServiceTest {
 
     @Mock
     private SpotifyApi spotifyApi;
