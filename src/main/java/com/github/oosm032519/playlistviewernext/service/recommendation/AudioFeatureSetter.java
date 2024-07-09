88ac97881b44f9aad6bf9e9896915ed6dee2d58e
oosm032519 <ryooshima0519@gmail.com>
2024-07-09 20:03:04 +0900

    SpotifyRecommendationServiceを分割

---
+++ src/main/java/com/github/oosm032519/playlistviewernext/service/recommendation/AudioFeatureSetter.java
@@ -0,0 +1,103 @@
+package com.github.oosm032519.playlistviewernext.service.recommendation;
+
+import org.springframework.stereotype.Component;
+import se.michaelthelin.spotify.requests.data.browse.GetRecommendationsRequest;
+
+import java.util.Map;
+
+@Component
+public class AudioFeatureSetter {
+
+    public void setMaxAudioFeatures(GetRecommendationsRequest.Builder builder, Map<String, Float> maxAudioFeatures) {
+        if (maxAudioFeatures.containsKey("danceability")) {
+            builder.max_danceability(maxAudioFeatures.get("danceability"));
+        }
+        if (maxAudioFeatures.containsKey("energy")) {
+            builder.max_energy(maxAudioFeatures.get("energy"));
+        }
+        if (maxAudioFeatures.containsKey("valence")) {
+            builder.max_valence(maxAudioFeatures.get("valence"));
+        }
+        if (maxAudioFeatures.containsKey("tempo")) {
+            builder.max_tempo(maxAudioFeatures.get("tempo"));
+        }
+        if (maxAudioFeatures.containsKey("acousticness")) {
+            builder.max_acousticness(maxAudioFeatures.get("acousticness"));
+        }
+        if (maxAudioFeatures.containsKey("instrumentalness")) {
+            builder.max_instrumentalness(maxAudioFeatures.get("instrumentalness"));
+        }
+        if (maxAudioFeatures.containsKey("liveness")) {
+            builder.max_liveness(maxAudioFeatures.get("liveness"));
+        }
+        if (maxAudioFeatures.containsKey("speechiness")) {
+            builder.max_speechiness(maxAudioFeatures.get("speechiness"));
+        }
+    }
+
+    public void setMinAudioFeatures(GetRecommendationsRequest.Builder builder, Map<String, Float> minAudioFeatures) {
+        if (minAudioFeatures.containsKey("danceability")) {
+            builder.min_danceability(minAudioFeatures.get("danceability"));
+        }
+        if (minAudioFeatures.containsKey("energy")) {
+            builder.min_energy(minAudioFeatures.get("energy"));
+        }
+        if (minAudioFeatures.containsKey("valence")) {
+            builder.min_valence(minAudioFeatures.get("valence"));
+        }
+        if (minAudioFeatures.containsKey("tempo")) {
+            builder.min_tempo(minAudioFeatures.get("tempo"));
+        }
+        if (minAudioFeatures.containsKey("acousticness")) {
+            builder.min_acousticness(minAudioFeatures.get("acousticness"));
+        }
+        if (minAudioFeatures.containsKey("instrumentalness")) {
+            builder.min_instrumentalness(minAudioFeatures.get("instrumentalness"));
+        }
+        if (minAudioFeatures.containsKey("liveness")) {
+            builder.min_liveness(minAudioFeatures.get("liveness"));
+        }
+        if (minAudioFeatures.containsKey("speechiness")) {
+            builder.min_speechiness(minAudioFeatures.get("speechiness"));
+        }
+    }
+
+    public void setMedianAudioFeatures(GetRecommendationsRequest.Builder builder, Map<String, Float> medianAudioFeatures) {
+        if (medianAudioFeatures.containsKey("danceability")) {
+            builder.target_danceability(medianAudioFeatures.get("danceability"));
+        }
+        if (medianAudioFeatures.containsKey("energy")) {
+            builder.target_energy(medianAudioFeatures.get("energy"));
+        }
+        if (medianAudioFeatures.containsKey("valence")) {
+            builder.target_valence(medianAudioFeatures.get("valence"));
+        }
+        if (medianAudioFeatures.containsKey("tempo")) {
+            builder.target_tempo(medianAudioFeatures.get("tempo"));
+        }
+        if (medianAudioFeatures.containsKey("acousticness")) {
+            builder.target_acousticness(medianAudioFeatures.get("acousticness"));
+        }
+        if (medianAudioFeatures.containsKey("instrumentalness")) {
+            builder.target_instrumentalness(medianAudioFeatures.get("instrumentalness"));
+        }
+        if (medianAudioFeatures.containsKey("liveness")) {
+            builder.target_liveness(medianAudioFeatures.get("liveness"));
+        }
+        if (medianAudioFeatures.containsKey("speechiness")) {
+            builder.target_speechiness(medianAudioFeatures.get("speechiness"));
+        }
+    }
+
+    public void setModeValues(GetRecommendationsRequest.Builder builder, Map<String, Object> modeValues) {
+        if (modeValues.containsKey("key")) {
+            builder.target_key((Integer) modeValues.get("key"));
+        }
+        if (modeValues.containsKey("mode")) {
+            builder.target_mode("MAJOR".equals(modeValues.get("mode")) ? 1 : 0);
+        }
+        if (modeValues.containsKey("time_signature")) {
+            builder.target_time_signature((Integer) modeValues.get("time_signature"));
+        }
+    }
+}
--- src/main/java/com/github/oosm032519/playlistviewernext/service/recommendation/SpotifyRecommendationService.java
+++ src/main/java/com/github/oosm032519/playlistviewernext/service/recommendation/SpotifyRecommendationService.java
@@ -21,10 +21,12 @@ import java.util.Map;
 public class SpotifyRecommendationService {
     private static final Logger logger = LoggerFactory.getLogger(SpotifyRecommendationService.class);
     private final SpotifyApi spotifyApi;
+    private final AudioFeatureSetter audioFeatureSetter;
 
     @Autowired
-    public SpotifyRecommendationService(SpotifyApi spotifyApi) {
+    public SpotifyRecommendationService(SpotifyApi spotifyApi, AudioFeatureSetter audioFeatureSetter) {
         this.spotifyApi = spotifyApi;
+        this.audioFeatureSetter = audioFeatureSetter;
     }
 
     public List<Track> getRecommendations(List<String> seedGenres, Map<String, Float> maxAudioFeatures, Map<String, Float> minAudioFeatures, Map<String, Float> medianAudioFeatures, Map<String, Object> modeValues) throws IOException, SpotifyWebApiException, ParseException {
@@ -38,94 +40,10 @@ public class SpotifyRecommendationService {
                 .seed_genres(genres)
                 .limit(20);
 
-        // 最大オーディオフィーチャーを設定
-        if (maxAudioFeatures.containsKey("danceability")) {
-            recommendationsRequestBuilder.max_danceability(maxAudioFeatures.get("danceability"));
-        }
-        if (maxAudioFeatures.containsKey("energy")) {
-            recommendationsRequestBuilder.max_energy(maxAudioFeatures.get("energy"));
-        }
-        if (maxAudioFeatures.containsKey("valence")) {
-            recommendationsRequestBuilder.max_valence(maxAudioFeatures.get("valence"));
-        }
-        if (maxAudioFeatures.containsKey("tempo")) {
-            recommendationsRequestBuilder.max_tempo(maxAudioFeatures.get("tempo"));
-        }
-        if (maxAudioFeatures.containsKey("acousticness")) {
-            recommendationsRequestBuilder.max_acousticness(maxAudioFeatures.get("acousticness"));
-        }
-        if (maxAudioFeatures.containsKey("instrumentalness")) {
-            recommendationsRequestBuilder.max_instrumentalness(maxAudioFeatures.get("instrumentalness"));
-        }
-        if (maxAudioFeatures.containsKey("liveness")) {
-            recommendationsRequestBuilder.max_liveness(maxAudioFeatures.get("liveness"));
-        }
-        if (maxAudioFeatures.containsKey("speechiness")) {
-            recommendationsRequestBuilder.max_speechiness(maxAudioFeatures.get("speechiness"));
-        }
-
-        // 最小オーディオフィーチャーを設定
-        if (minAudioFeatures.containsKey("danceability")) {
-            recommendationsRequestBuilder.min_danceability(minAudioFeatures.get("danceability"));
-        }
-        if (minAudioFeatures.containsKey("energy")) {
-            recommendationsRequestBuilder.min_energy(minAudioFeatures.get("energy"));
-        }
-        if (minAudioFeatures.containsKey("valence")) {
-            recommendationsRequestBuilder.min_valence(minAudioFeatures.get("valence"));
-        }
-        if (minAudioFeatures.containsKey("tempo")) {
-            recommendationsRequestBuilder.min_tempo(minAudioFeatures.get("tempo"));
-        }
-        if (minAudioFeatures.containsKey("acousticness")) {
-            recommendationsRequestBuilder.min_acousticness(minAudioFeatures.get("acousticness"));
-        }
-        if (minAudioFeatures.containsKey("instrumentalness")) {
-            recommendationsRequestBuilder.min_instrumentalness(minAudioFeatures.get("instrumentalness"));
-        }
-        if (minAudioFeatures.containsKey("liveness")) {
-            recommendationsRequestBuilder.min_liveness(minAudioFeatures.get("liveness"));
-        }
-        if (minAudioFeatures.containsKey("speechiness")) {
-            recommendationsRequestBuilder.min_speechiness(minAudioFeatures.get("speechiness"));
-        }
-
-        // 中央オーディオフィーチャーを設定
-        if (medianAudioFeatures.containsKey("danceability")) {
-            recommendationsRequestBuilder.target_danceability(medianAudioFeatures.get("danceability"));
-        }
-        if (medianAudioFeatures.containsKey("energy")) {
-            recommendationsRequestBuilder.target_energy(medianAudioFeatures.get("energy"));
-        }
-        if (medianAudioFeatures.containsKey("valence")) {
-            recommendationsRequestBuilder.target_valence(medianAudioFeatures.get("valence"));
-        }
-        if (medianAudioFeatures.containsKey("tempo")) {
-            recommendationsRequestBuilder.target_tempo(medianAudioFeatures.get("tempo"));
-        }
-        if (medianAudioFeatures.containsKey("acousticness")) {
-            recommendationsRequestBuilder.target_acousticness(medianAudioFeatures.get("acousticness"));
-        }
-        if (medianAudioFeatures.containsKey("instrumentalness")) {
-            recommendationsRequestBuilder.target_instrumentalness(medianAudioFeatures.get("instrumentalness"));
-        }
-        if (medianAudioFeatures.containsKey("liveness")) {
-            recommendationsRequestBuilder.target_liveness(medianAudioFeatures.get("liveness"));
-        }
-        if (medianAudioFeatures.containsKey("speechiness")) {
-            recommendationsRequestBuilder.target_speechiness(medianAudioFeatures.get("speechiness"));
-        }
-
-        // modeValuesの設定
-        if (modeValues.containsKey("key")) {
-            recommendationsRequestBuilder.target_key((Integer) modeValues.get("key"));
-        }
-        if (modeValues.containsKey("mode")) {
-            recommendationsRequestBuilder.target_mode("MAJOR".equals(modeValues.get("mode")) ? 1 : 0);
-        }
-        if (modeValues.containsKey("time_signature")) {
-            recommendationsRequestBuilder.target_time_signature((Integer) modeValues.get("time_signature"));
-        }
+        audioFeatureSetter.setMaxAudioFeatures(recommendationsRequestBuilder, maxAudioFeatures);
+        audioFeatureSetter.setMinAudioFeatures(recommendationsRequestBuilder, minAudioFeatures);
+        audioFeatureSetter.setMedianAudioFeatures(recommendationsRequestBuilder, medianAudioFeatures);
+        audioFeatureSetter.setModeValues(recommendationsRequestBuilder, modeValues);
 
         GetRecommendationsRequest recommendationsRequest = recommendationsRequestBuilder.build();
         Recommendations recommendations = recommendationsRequest.execute();
