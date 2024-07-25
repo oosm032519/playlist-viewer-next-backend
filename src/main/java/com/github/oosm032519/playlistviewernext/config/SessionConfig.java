d1a69d036ffe7e43c3e39453aa85f2cc1957268c
oosm032519 <ryooshima0519@gmail.com>
2024-07-25 15:07:09 +0900

    LettuceConnectionFactoryを修正

--- src/main/java/com/github/oosm032519/playlistviewernext/config/SessionConfig.java
+++ src/main/java/com/github/oosm032519/playlistviewernext/config/SessionConfig.java
@@ -1,26 +1,36 @@
 package com.github.oosm032519.playlistviewernext.config;
 
-import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 @EnableRedisHttpSession
 public class SessionConfig {
 
-    @Bean
-    public LettuceConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory();
-    }
+    @Value("${REDIS_TLS_URL}")
+    private String redisUrl;
 
     @Bean
-    public LettuceClientConfigurationBuilderCustomizer lettuceClientConfigurationBuilderCustomizer() {
-        return clientConfigurationBuilder -> {
-            if (clientConfigurationBuilder.build().isUseSsl()) {
-                clientConfigurationBuilder.useSsl().disablePeerVerification();
-            }
-        };
+    public LettuceConnectionFactory connectionFactory() throws URISyntaxException {
+        URI redisUri = new URI(redisUrl);
+
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(redisUri.getHost());
+        redisConfig.setPort(redisUri.getPort());
+        redisConfig.setPassword(redisUri.getUserInfo().split(":", 2)[1]);
+
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .useSsl().disablePeerVerification()
+                .build();
+
+        return new LettuceConnectionFactory(redisConfig, clientConfig);
     }
 }
