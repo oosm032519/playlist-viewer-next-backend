9f0498401995de683a4149cc949cb0eef6f7dd5f
oosm032519 <ryooshima0519@gmail.com>
2024-08-07 14:34:05 +0900

    TLSピア検証を無効

--- src/main/java/com/github/oosm032519/playlistviewernext/config/SecurityConfig.java
+++ src/main/java/com/github/oosm032519/playlistviewernext/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -192,5 +193,14 @@ public class SecurityConfig {
     @Bean
     public SpotifyOAuth2UserService spotifyOAuth2UserService() {
         return new SpotifyOAuth2UserService();
+    }
+
+    @Bean
+    public LettuceClientConfigurationBuilderCustomizer lettuceClientConfigurationBuilderCustomizer() {
+        return clientConfigurationBuilder -> {
+            if (clientConfigurationBuilder.build().isUseSsl()) {
+                clientConfigurationBuilder.useSsl().disablePeerVerification();
+            }
+        };
     }
 }
