4e8871cc52242e4b651a6c171b97e87069a96137
oosm032519 <ryooshima0519@gmail.com>
2024-08-07 19:20:32 +0900

    Cookie送信テストを追加

---
+++ src/main/java/com/github/oosm032519/playlistviewernext/controller/TestCookieController.java
@@ -0,0 +1,29 @@
+// com.github.oosm032519.playlistviewernext.controller.TestCookieController.java
+package com.github.oosm032519.playlistviewernext.controller;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class TestCookieController {
+
+    @GetMapping("/test-cookie")
+    public ResponseEntity<Map<String, String>> setTestCookie(HttpServletResponse response) {
+        Cookie cookie = new Cookie("testCookie", "testValue");
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+        Map<String, String> responseBody = new HashMap<>();
+        responseBody.put("testCookie", "testValue");
+        return ResponseEntity.ok(responseBody);
+    }
+}
